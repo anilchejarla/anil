c491cd0454dfdda130f0abac28d292580e2059a6
anilchejarla <anilchejarla205@gmail.com>
2017-07-31 14:13:18 +0530

    addition commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+static int a=78999, b=5117,add=a+b;
+public static void main(String[] ani)
+{
+System.out.println("The values of a and b are stored in the variable c....");
+System.out.println("So the value of c is...:"+add);
+}
+}
+
